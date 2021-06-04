e5b8647f0f9a9abf510042d47a9ca03de641a6b4
jinmirae <mirae583@naver.com>
2021-06-04 11:39:18 +0900

    ㅇㄴㄴㅇㄹ

--- src/test/java/com/edu/test/DataSourceTest.java
+++ src/test/java/com/edu/test/DataSourceTest.java
@@ -49,7 +49,7 @@ public class DataSourceTest {
 		}
 		//인서트, 업데이트, 삭제시 sql 디벨러퍼는 커밋이 필수지만, 외부 java클래스에서는 걍 자동됨
 		// 테이블에 입력되어 있는 레코드를 select 쿼리 stmt로 가져옴
-		ResultSet rs = stmt.executeQuery("select * from dept02");
+		ResultSet rs = stmt.executeQuery("select * from dept02 order by deptno");
 		//위에서 저장된 rs객체를 반복문으로 출력 (아래)
 		while(rs.next()) { //rs객체의 레코드가 없을때 까지 반복
 		logger.debug(rs.getString("deptno")+" "+rs.getString("dname")+" "+rs.getString("loc"));
