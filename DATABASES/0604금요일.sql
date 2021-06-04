6f2caaf6603a8ece1679706592ba5f072b804537
skdi262 <skdi262@naver.com>
2021-06-04 15:25:55 +0900

    가느다란마ㅓㅂ사

--- DATABASES/0604금요일.sql
+++ DATABASES/0604금요일.sql
@@ -1,3 +1,19 @@
+-- 8장 함수.
+-- 부서별 연봉의 합계
+-- DB 셋팅에서 대소문자 구분해서 사용할지, 구분하지 않을 지 셋팅.
+
+
+select * from (select deptno, sum(sal) as "DEPT_SAL"
+from emp group by deptno) order by dept_sal DESC;
+select ename,round(sal,-3) from emp;
+select sum(sal) from emp;
+select round(avg(sal)) from emp;
+select MAX(sal), Min(sal) from emp;
+select MAX(sal), MIN(sal), MAX(sal)-MIN(sal) as "대표와 사원의 연봉차" FROM emp;
+select COUNT(*) from emp WHERE sal >= 
+(select round(avg(sal)) from emp);
+
+
 --DDL 문(create; alter;), DCL문(commit;rollback;)
 --DML 문 (Data Maufacture Language), insert, update, delete
 --insert문: 테이블에 새로운 레코드(row) 추가
@@ -14,3 +30,14 @@ SELECT * FROM dept02 order by deptno;
 --insert into dept02 values(20,'디자인부','경기도')
 delete from dept02 where deptno >= 0;
 --delete from dept02; -- 이렇게 작업하면사용은 되는데 모든 레코드가 삭제되서 안됨.
+--drop table 테이블명은 테이블자체를 물리적으로 없애는 명령
+drop TABLE dept02; --drop 테이블은 커밋필요없이 바로 걍 사라짐 ㅠㅠ 슬펑
+create table emp01 as SELECT * from emp;
+select * from emp01;
+-- UPDATE 테이블명 SET 필드명 = '바꿀값' where empno = '특정id값' 해줘야댐
+UPDATE emp01 SET ename = '김성중' where empno = 7839;
+ROLLBACK;
+--ROLLBACK은 마지막 커밋 전 까지 되돌아감.
+UPDATE emp01 SET sal = sal*110/100; 
+-- 모든 직원의 연봉을 10프로 인상
+UPDATE emp01 SET hiredate = sysdate;
--- src/test/java/com/edu/test/DataSourceTest.java
+++ src/test/java/com/edu/test/DataSourceTest.java
@@ -32,7 +32,7 @@ public class DataSourceTest {
 	private Logger logger = Logger.getLogger(DataSourceTest.class);
 	//dataSource 객체는 데이터베이스 객체를 pool로 저장해서 사용할때 DataSource 클래스를 사용
 	@Inject //인젝트는 스프링에서 객체를 만드는 방법
-	DataSource dataSource; //Inject로 객체를 만들면 메모리 관리를 스프링이 대신해줌.
+	DataSource dataSource; //Inject로 객체를 만들면 메모리 관리를 스프링이 대신해줌
 	@Test 
 	public void oldQueryTest() throws Exception{
 		//스프링빈을 사용하지 않을때 예전 방식 : 코테는 스프링설정을 안 쓰고 , 직접 DB의 ID/암호를 입력
@@ -44,18 +44,22 @@ public class DataSourceTest {
 		//보안상 이유로 퀴리문장을 따로 만드는 거임 .
 		//stmt 객체가 없으면, 개발자가 SQL인젝션 방지코드를 넣어야함
 		//Insert쿼리문장 만들기.
-		for(int cnt=0;cnt<100;cnt++){
-		stmt.executeQuery("insert into dept02 values("+cnt+",'디자인부','경기도')");
-		}
+		/*
+		 * for(int cnt=0;cnt<100;cnt++){
+		 * stmt.executeQuery("insert into dept02 values("+cnt+",'디자인부','경기도')"); }
+		 */
 		//인서트, 업데이트, 삭제시 sql 디벨러퍼는 커밋이 필수지만, 외부 java클래스에서는 걍 자동됨
 		// 테이블에 입력되어 있는 레코드를 select 쿼리 stmt로 가져옴
-		ResultSet rs = stmt.executeQuery("select * from dept02 order by deptno");
+		ResultSet rs = stmt.executeQuery("select * from dept order by deptno");
 		//위에서 저장된 rs객체를 반복문으로 출력 (아래)
 		while(rs.next()) { //rs객체의 레코드가 없을때 까지 반복
 		logger.debug(rs.getString("deptno")+" "+rs.getString("dname")+" "+rs.getString("loc"));
 		}
-		connection=null;//메모리 초기화
+		stmt = null;
+		rs = null;
+		connection = null;//메모리 초기화
 	}
+	
 	@Test
 	public void dbConnectionTest() throws Exception{
 		//데이터 베이스 커넥션 테스트 : 설정은 root-context의 빈(스프링클래스)를 이용
